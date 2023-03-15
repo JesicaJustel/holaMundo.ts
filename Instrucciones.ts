f311f3c71a795cff25b3cc34bdf4b9cdf04550b8
JesicaJustel <127353366+JesicaJustel@users.noreply.github.com>
2023-03-15 15:37:24 -0300

    Add files via upload

---
+++ Instrucciones.ts
@@ -0,0 +1,5 @@
+console.log("instrucciones para hacer una torta.ts");
+console.log("1 paso:en un bol, poner harina");
+console.log("2 paso:agregar huevos con azucar");
+console.log("3 paso:mezclar todo con leche");
+console.log("4 paso:pasar la preparacion al molde y hornear");
